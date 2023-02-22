b494e9bf780e2add73ce50b349f257b0831f5efe
renwen <renwen,de@163.com>
2023-02-22 16:35:53 +0800

    第二次提交

---
+++ pythonProject/APITestCases/test_cases_API.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+import pytest
+
+
+class TestLogin():
+    @pytest.mark.run(order=3)
+    @pytest.mark.smoke
+    def test_login1(self):
+        print("firstlogin")
+
+    @pytest.mark.run(order=1)
+    @pytest.mark.aa
+    @pytest.mark.smoke
+    def test_login2(self):
+        print("secondlogin")
+        assert  1==2
+
+
+    @pytest.mark.run(order=2)
+    def test_login3(self):
+        print("thirdlogin")
---
+++ pythonProject/UITestCaes/test_cases_UI.py
@@ -0,0 +1,10 @@
+class TestRegist():
+
+    def test_regist1(self):
+        print("firstregist")
+
+    def test_regist2(self):
+        print("secondregist")
+
+    def test_regist3(self):
+        print("thirdregist")
